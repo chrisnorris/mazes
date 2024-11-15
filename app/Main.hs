750b912b245143e75efd29285b03e2ae5ffc2aa8
chrisnorris <chrisnorris@users.noreply.github.com>
2024-11-15 15:27:45 +0000

    automated ormolu commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,34 +2,31 @@ module Main where
 
 import Data.Bifunctor (bimap)
 import Data.Functor ((<&>))
+import Data.Vector.Mutable qualified as VM
+import Data.Vector.Unboxed ((!), (//))
+import Data.Vector.Unboxed qualified as V
 import Graphics.Gloss
-
-import           Data.Vector.Unboxed ((!), (//))
-import qualified Data.Vector.Unboxed as V
-
-import qualified Data.Vector.Mutable as VM
-
-import qualified Graphics.Gloss.Data.Point.Arithmetic as Pt
-import qualified Graphics.Gloss.Data.Color(white)
+import Graphics.Gloss.Data.Color qualified (white)
+import Graphics.Gloss.Data.Point.Arithmetic qualified as Pt
 import Graphics.Gloss.Interface.IO.Simulate
-    ( greyN,
-      polygon,
-      simulateIO,
-      makeColor,
-      Display(InWindow),
-      Color,
-      Picture(ThickCircle, Pictures, Color, Translate),
-      Point )
+  ( Color,
+    Display (InWindow),
+    Picture (Color, Pictures, ThickCircle, Translate),
+    Point,
+    greyN,
+    makeColor,
+    polygon,
+    simulateIO,
+  )
 import System.Random
-
 
 width :: Float
 width = 32
 
-type RGBAofColor = (Float,Float,Float,Float)
+type RGBAofColor = (Float, Float, Float, Float)
 
 data Maze = Maze
-  { --whiteBall :: Ball,
+  { -- whiteBall :: Ball,
     -- baseCells :: [(RGBAofColor, (Float, Float))],
     baseCellsUV :: V.Vector (RGBAofColor, (Float, Float))
   }
@@ -39,7 +36,7 @@ main = do
   directions <- getStdGen <&> take 4 . randomRs (0, 360)
   simulateIO
     (InWindow "Maze - algorithms" (1800, 1600) (100, 100))
-    white 
+    white
     100 -- number of steps per second, bigger => faster
     ( startGame
         directions
@@ -49,42 +46,40 @@ main = do
 
 startGame :: [Float] -> Maze
 startGame dirs =
-  let 
-      cells = [(x - width/2 ,y-width/2) | x<- [0..width], y <- [0..width]]
-   in Maze {
-            baseCellsUV = V.fromList $ (,) `zipWith` repeat (rgbaOfColor black) $ cells}
+  let cells = [(x - width / 2, y - width / 2) | x <- [0 .. width], y <- [0 .. width]]
+   in Maze
+        { baseCellsUV = V.fromList $ (,) `zipWith` repeat (rgbaOfColor black) $ cells
+        }
 
 stepWorld :: p -> Float -> Maze -> IO Maze
-stepWorld _ t y@(Maze baseCellsUV) = 
+stepWorld _ t y@(Maze baseCellsUV) =
   do
-   mycolorRaw <- randomRGBRaw
-   [i] :: [Int] <-  newStdGen <&> take 1 . randomRs (0, round width ^2)
-   let (colUV, coordsUV) = baseCellsUV ! i
-  --  let newCells = take i baseCells <> [(mycolorRaw, coords)] <> drop (i + 1) baseCells
-   let newCellsUV = baseCellsUV // [(i, (mycolorRaw, coordsUV))]
-   return $ Maze newCellsUV
+    mycolorRaw <- randomRGBRaw
+    [i] :: [Int] <- newStdGen <&> take 1 . randomRs (0, round width ^ 2)
+    let (colUV, coordsUV) = baseCellsUV ! i
+    --  let newCells = take i baseCells <> [(mycolorRaw, coords)] <> drop (i + 1) baseCells
+    let newCellsUV = baseCellsUV // [(i, (mycolorRaw, coordsUV))]
+    return $ Maze newCellsUV
 
 renderToPicture :: Maze -> IO Picture
 -- renderToPicture (Maze whiteBall@(Ball p v) baseCellsUV) =
 renderToPicture (Maze baseCellsUV) = do
-
   -- [i] :: [Int] <-  newStdGen <&> take 1 . randomRs (0, 3)
-  return $ Color black $
-    Pictures $
-        
-        let s = 1000/width in
-        map
-          ( \(c@(r,g,bl,z),(a, b)) -> if c == rgbaOfColor black then 
-
-              Color black $ Translate (a*s) (b*s) (line [(0,0), (0,s), (s, s), (s,0), (0,0)] )
-              else
-              Color (makeColor r g bl z) $ Translate (a*s) (b*s)  (polygon [(0,0), (0,s), (s, s), (s,0), (0,0)] )
-          )
-          (V.toList baseCellsUV)
+  return $
+    Color black $
+      Pictures $
+        let s = 1000 / width
+         in map
+              ( \(c@(r, g, bl, z), (a, b)) ->
+                  if c == rgbaOfColor black
+                    then Color black $ Translate (a * s) (b * s) (line [(0, 0), (0, s), (s, s), (s, 0), (0, 0)])
+                    else Color (makeColor r g bl z) $ Translate (a * s) (b * s) (polygon [(0, 0), (0, s), (s, s), (s, 0), (0, 0)])
+              )
+              (V.toList baseCellsUV)
 
 randomRGB :: IO Color
 randomRGB = do
-  [r,b,g] :: [Float] <- newStdGen <&> take 3 . randomRs (0, 0.99999)
+  [r, b, g] :: [Float] <- newStdGen <&> take 3 . randomRs (0, 0.99999)
   return $ makeColor r b g 1.0
 
 randomRGBRaw :: IO RGBAofColor
